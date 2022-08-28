4567092453f2cfb51a8158fe2098b8a6cb3cb649
ootsubo1192 <ootubo_stock@yahoo.co.jp>
2022-08-28 14:09:42 +0900

    delete put-rule.sh

--- EventBridge/put-rule.sh
+++
@@ -1,8 +0,0 @@
-
-# put-event aws cloudwatch notification issue
-aws events put-rule --name test-cli --event-pattern "{   \"source\": [\"aws.health\"],   \"detail-type\": [\"AWS Health Event\"],   \"detail\": {     \"service\": [\"CLOUDWATCH\"],     \"eventTypeCategory\": [\"issue\"]   } }" --state ENABLED --description test-cli
-
-# put-target aws cloudwatch notification issue
-aws events put-targets --rule test-cli --targets "Id"="Target1","Arn"="arn:aws:sns:ap-northeast-1:817023884338:test-cli","InputTransformer":"{\"InputPathsMap\": {\"EventTypeCategory\": \"$.detail.eventTypeCategory\"},\"InputTemplate\": \"<EventTypeCategory>\"}"
-
-
